7c162bb1d9b22d57d31fe842d8f2acaf002c3dae
Rodrigo Fernando Gomes Lima <rfergomes@gmail.com>
2026-06-02 16:29:55 -0300

    feat: create users list view with toggleable card and table layouts

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,7 +9,7 @@
 @section('content')
     <div class="row">
         <div class="col-12">
-            <div class="card shadow-sm mb-4">
+            <div class="card shadow-sm bg-transparent mb-4">
                 <div class="card-header border-0 bg-white py-3">
                     <div class="d-flex justify-content-between align-items-center w-100">
                         <h5 class="mb-0 text-secondary">
